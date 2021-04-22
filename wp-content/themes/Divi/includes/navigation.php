82781cca412702911c2d0651f93cc7793b4ef5ff
Kevin Midbøe <kevin.midboe@gmail.com>
2021-04-22 17:57:16 +0200

    Initial commit. State 04.2021.

---
+++ wp-content/themes/Divi/includes/navigation.php
@@ -0,0 +1,12 @@
+<div class="pagination clearfix">
+	<div class="alignleft"><?php next_posts_link(esc_html__('&laquo; Older Entries','Divi')); ?></div>
+	<div class="alignright"><?php previous_posts_link(esc_html__('Next Entries &raquo;', 'Divi')); ?></div>
+</div>
+
+<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="logo" />
+</a>
+
+<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+<img src="2nd logo url" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="logo2" />
+</a>
